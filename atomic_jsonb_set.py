153ea71a57eeb60180142957364201aed0079ae6
Amit Shah <amit@amwam.me>
2022-03-08 16:59:51 +0000

    Working solution
    Signed-off-by: Amit Shah <amit@amwam.me>

--- atomic_jsonb_set.py
+++ atomic_jsonb_set.py
@@ -8,34 +8,24 @@ from models import DeclarativeBase, DBSession
 
 
 def atomic_jsonb_set(
-    context: DeclarativeBase,
-    dynamic_column_name: str,
-    column_id,
-    value,
+    context: DeclarativeBase, dynamic_column_name: str, column_id, value,
 ):
     mapper = inspect(context.__class__)
     dynamic_column = getattr(context.__class__, dynamic_column_name)
-    query = DBSession.query(dynamic_column)
 
+    # We need to construct the query against the parent table
+    query = DBSession.query(mapper.base_mapper.class_)
 
-    try:
-        # needed for inheritance (persist_selectable is a Join)
-        query = query.filter(mapper.persist_selectable.onclause)
-    except AttributeError:
-        # not a child class
-        pass
-
-    # filter by primary key
-    id_key = [col for col in mapper.primary_key]
-    for col in id_key:
-        query = query.filter(col == getattr(context, col.name))
+    # Filter by all the primary keys on the table
+    for pk_col, pk_value in zip(
+        mapper.primary_key, mapper.primary_key_from_instance(context)
+    ):
+        query = query.filter(pk_col == pk_value)
 
     query.update(
         {
             dynamic_column: func.jsonb_set(
-                dynamic_column,
-                f"{{{column_id}}}",
-                json.dumps(value),
+                dynamic_column, f"{{{column_id}}}", json.dumps(value),
             )
         },
         synchronize_session="fetch",
